c6884872cb54a45dc2ccbe3e777058aa514f02fe
AiriaudF <airiaud.florian@gmail.com>
2013-11-02 23:42:40 +0100

    Create Exercice1.js

---
+++ Exercice1.js
@@ -0,0 +1,31 @@
+function adder(l, r) {
+       return function (v) {
+                return l(v) + r(v);
+       }        
+}
+
+function mult(v) {
+        return function (e) {
+                return v*e;
+        }
+}
+
+//Function foo 
+function foo (l) {
+        return function (v) {
+                return l - v;
+        }        
+}
+
+
+//adder function 
+function adder () {
+        var tab = Array.prototype.slice.call(arguments);
+        var res = 0;
+          return function(v) {
+                tab.forEach(function(foo) {                                
+                        res += foo(v);
+                });                        
+                return res;              
+        };
+}
