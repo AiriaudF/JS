fed8db5c7447e0d6492c0596e271957e51734f8b
AiriaudF <airiaud.florian@gmail.com>
2013-11-02 23:52:18 +0100

    Update Exercice1.js

--- Exercice1.js
+++ Exercice1.js
@@ -1,12 +1,12 @@
 function adder(l, r) {
        return function (v) {
-                return l(v) + r(v);
+            return l(v) + r(v);
        }        
 }
 
 function mult(v) {
         return function (e) {
-                return v*e;
+            return v*e;
         }
 }
 
@@ -23,9 +23,9 @@ function adder () {
         var tab = Array.prototype.slice.call(arguments);
         var res = 0;
           return function(v) {
-                tab.forEach(function(foo) {                                
-                        res += foo(v);
-                });                        
-                return res;              
+            tab.forEach(function(foo) {                                
+                res += foo(v);
+            });                        
+            return res;              
         };
 }
